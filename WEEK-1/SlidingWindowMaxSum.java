b39288d08acf94817a371ca46d647191b3239b8f
sachin-0208 <sachinchoudhary04082@gmail.com>
2025-02-08 01:06:47 +0530

    Implement edge case handling and optimize sliding window sum calculation

---
+++ WEEK-1/SlidingWindowMaxSum.java
@@ -0,0 +1,21 @@
+class SlidingWindowMaxSum{
+    static int slidingWindowMaxSum(int[] a,int k){
+        if(k<=0 || k>a.length || a.length==0) return -1;
+        int sum = 0;
+        for(int i=0;i<k;i++){
+            sum+=a[i];
+        }
+        int maxsum = sum;
+        for(int i=k;i<a.length;i++){
+            sum += a[i]-a[i-k];
+            maxsum = Math.max(maxsum,sum);
+        }
+        return maxsum;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,5,4,-3,2,-4,2,1};
+        int k=3;
+        System.out.println(slidingWindowMaxSum(arr,k));
+
+    }
+}
