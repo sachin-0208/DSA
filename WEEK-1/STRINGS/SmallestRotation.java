acc459274e6091fbea987386a6c37553c9bcda7d
sachin-0208 <sachinchoudhary04082@gmail.com>
2025-02-09 16:14:49 +0530

    Minor issue fix

--- WEEK-1/STRINGS/SmallestRotation.java
+++ WEEK-1/STRINGS/SmallestRotation.java
@@ -1,13 +1,20 @@
-class SmallestRotation{
-    static String smallestRotation(String s){
+class SmallestRotation {
+    static String smallestRotation(String s) {
         int n = s.length();
         String sr = s;
-        for(int i=1;i<n;i++){
-            String temp = s.substring(i)+s.substring(0,i);
-            if(temp.compareTo(sr)<0){
+        for (int i = 1; i < n; i++) {
+            String temp = s.substring(i) + s.substring(0, i);
+            if (temp.compareTo(sr) < 0) {
                 sr = temp;
             }
         }
         return sr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(smallestRotation("dcbaabcd"));  
+        System.out.println(smallestRotation("cbabc"));     
+        System.out.println(smallestRotation("aaaaa"));     
+        System.out.println(smallestRotation("z"));         
     }
 }
