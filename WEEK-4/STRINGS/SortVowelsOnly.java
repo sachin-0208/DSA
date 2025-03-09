959bdc5a5b8b6abbb0a11ef1967379ddd206cd24
sachin-0208 <sachinchoudhary04082@gmail.com>
2025-03-09 13:12:24 +0530

    Implement a program to sort only the vowels

---
+++ WEEK-4/STRINGS/SortVowelsOnly.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class SortVowelsOnly {
+    public String sortVowels(String s) {
+        List<Character> list = new ArrayList<>();
+        for(char c:s.toCharArray()){
+            if("AEIOUaeiou".indexOf(c)!=-1) list.add(c);
+        }
+        Collections.sort(list);
+        int index = 0;
+        StringBuilder ss = new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(isVowel(c)){
+                ss.append(list.get(index++));
+            }
+            else{
+                ss.append(c);
+            }
+        }
+        return ss.toString();
+    }
+    public boolean isVowel(char c){
+        return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U') ? true:false;
+    }
+}
+
+/*
+ * Given a 0-indexed string s, permute s to get a new string t such that:
+
+All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+Return the resulting string.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+
+ 
+
+Example 1:
+
+Input: s = "lEetcOde"
+Output: "lEOtcede"
+Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+Example 2:
+
+Input: s = "lYmpH"
+Output: "lYmpH"
+Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+
+ */
