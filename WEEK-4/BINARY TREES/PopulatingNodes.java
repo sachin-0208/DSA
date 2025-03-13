e029c37e310ab5fd6a45c49875600b3ea3da54c3
sachin-0208 <sachinchoudhary04082@gmail.com>
2025-03-13 21:29:39 +0530

    Implement a program to populate the nodes of a BT

--- WEEK-4/BINARY TREES/PopulatingNodes.java
+++ WEEK-4/BINARY TREES/PopulatingNodes.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 import org.w3c.dom.Node;
 
 public class PopulatingNodes {
@@ -17,6 +20,26 @@ public class PopulatingNodes {
         }
         return root;
     }
+    // if (root == null) return null;
+        
+    //     Queue<Node> queue = new LinkedList<>();
+    //     queue.add(root);
+        
+    //     while (!queue.isEmpty()) {
+    //         int size = queue.size();
+    //         Node prev = null;
+            
+    //         for (int i = 0; i < size; i++) {
+    //             Node node = queue.poll();
+    //             if (prev != null) prev.next = node;
+    //             prev = node;
+                
+    //             if (node.left != null) queue.add(node.left);
+    //             if (node.right != null) queue.add(node.right);
+    //         }
+    //     }
+    //      return root;
+    // }
 }
 
 /*
