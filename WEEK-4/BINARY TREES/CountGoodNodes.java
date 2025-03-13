8aea1c107e4c30bf470e7ff7c0674851acf97129
sachin-0208 <sachinchoudhary04082@gmail.com>
2025-03-13 21:51:18 +0530

    Implement a program to find number of good nodes

---
+++ WEEK-4/BINARY TREES/CountGoodNodes.java
@@ -0,0 +1,60 @@
+public class CountGoodNodes {
+    public int goodNodes(TreeNode root) {
+        return count(root, root.val);
+    }
+    public static int count(TreeNode root,int maxv){
+        if(root==null) return 0;
+        int count=0;
+        if(root.val>=maxv) count++;
+        int nm = Math.max(root.val,maxv);
+        count+=count(root.left,nm); 
+        count+=count(root.right,nm);
+        return count; 
+    }
+    public static int count(TreeNode root,int maxv){
+        if(root==null) return 0;
+        if(root.val<maxv){
+            return count(root.left,maxv)+count(root.right,maxv);
+        }
+        else{
+            return 1+count(root.left,root.val)+count(root.right,root.val);
+        }
+    }
+}
+/*
+ * Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+Example 2:
+
+
+
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+Example 3:
+
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+ 
+
+Constraints:
+
+The number of nodes in the binary tree is in the range [1, 10^5].
+Each node's value is between [-10^4, 10^4].
+ */
