f4a396857a003ad01ba27f86a160966ed2fc58aa
sachin-0208 <sachinchoudhary04082@gmail.com>
2025-03-19 20:00:00 +0530

    Implement a program to find all possible combinations

--- WEEK-5/BACKTRACKING/Combination.java
+++ WEEK-5/BACKTRACKING/Combination.java
@@ -1,5 +1,20 @@
 //https://leetcode.com/problems/combinations/description/
 import java.util.*;
 public class Combination {
-    
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res,new ArrayList<>(),n,1,k);
+        return res;
+    }
+    public void backtrack(List<List<Integer>> res,List<Integer> temp,int n,int start,int k){
+        if(temp.size()==k){
+         res.add(new ArrayList<>(temp));
+         return;
+        }
+        for(int i=start;i<=n;i++){
+            temp.add(i);
+            backtrack(res,temp,n,i+1,k);
+            temp.remove(temp.size()-1);
+        }
+    }
 }
