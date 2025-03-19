753fbdfb36486b547063d89b8b49ee61caabe518
sachin-0208 <sachinchoudhary04082@gmail.com>
2025-03-19 19:57:55 +0530

    Implement a program to find all possible distinct permutations of a string

---
+++ WEEK-5/BACKTRACKING/PermutationII.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/permutations-ii/description/
+import java.util.*;
+public class PermutationII {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res,new ArrayList<>(),new boolean[nums.length],nums);
+        return res;
+    }
+    public void backtrack(List<List<Integer>> res,List<Integer> temp,boolean[] used,int[] nums){
+        if(temp.size()==nums.length){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+            used[i]=true;
+            temp.add(nums[i]);
+            backtrack(res,temp,used,nums);
+            temp.remove(temp.size()-1);
+            used[i]=false;
+        }
+    }
+    
+}
